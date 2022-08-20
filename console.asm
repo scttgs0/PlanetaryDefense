0c72ece122965ae48c40f07531eb51704c7bb86c
Scott Giese <scttgs0@gmail.com>
2022-08-20 10:25:27 -0500

    Joystick/Keyboard input support

--- console.asm
+++ console.asm
@@ -4,7 +4,7 @@
 EndGame         .proc
                 jsr SoundOff            ; no sound 123
 
-_next1          lda JOYSTICK0           ; stick trigger
+_next1          lda InputFlags          ; stick trigger
                 and $10
                 ;and PTRIG0             ; mask w/paddle 0
                 ;and PTRIG1             ; mask w/paddle 1
@@ -12,7 +12,7 @@ _next1          lda JOYSTICK0           ; stick trigger
 
                 lda CONSOL              ; chk console
                 cmp #7                  ; any pushed?
-                beq _next1              ; No. loop here
+                beq _next1              ;   no. loop here
 
 _1              jmp Planet              ; restart game
 
