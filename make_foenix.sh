5966332f93fef1716ad225e0962e2017b855bfae
scttgs0 <scttgs0@gmail.com>
2023-05-29 18:06:16 -0500

    migrate to F256jr

--- make_foenix.sh
+++
@@ -1,9 +0,0 @@
-
-64tass  --m65816 \
-        --long-address \
-        --flat \
-        --nostart \
-        -o pdefense.pgx \
-        --list=pdefense.lst \
-        --labels=pdefense.lbl \
-        planetarydefense.asm
